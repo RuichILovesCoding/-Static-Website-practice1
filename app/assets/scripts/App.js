183d56b15b42b538fa6e4bf0018081f72f9c33f3
Ruichi Wu <wurc199733@gmail.com>
2020-03-28 19:54:50 +1100

    the development environment set up, ready to do

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,18 @@
+import css from "../styles/styles.css";
+
+if (module.hot) {
+  module.hot.accept();
+}
 console.log("bundle this js file");
+
+const obj = {
+  name: "Ruichi",
+  age: 12
+};
+
+const people = {
+  ...obj,
+  gender: "female"
+};
+
+console.log(people);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
 const path = require("path");
+const postCSSPlugins = [
+  require("postcss-nested"),
+  require("postcss-simple-vars"),
+  require("autoprefixer"),
+  require("postcss-import"),
+  require("postcss-nested"),
+  require("postcss-mixins"),
+  require("postcss-nested-import")
+];
 
 module.exports = {
   entry: "./app/assets/scripts/App.js",
@@ -7,5 +16,40 @@ module.exports = {
     path: path.resolve(__dirname, "app")
   },
   mode: "development",
-  watch: true
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          { loader: "postcss-loader", options: { plugins: postCSSPlugins } }
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "transform-object-rest-spread"
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  devServer: {
+    before: function(app, server) {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.resolve(__dirname, "app"),
+    hot: true,
+    host: "0.0.0.0"
+  }
 };
