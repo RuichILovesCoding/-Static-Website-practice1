6398415f4ef6f9f2335a72aeb230bac36a81716c
Ruichi Wu <wurc199733@gmail.com>
2020-04-16 16:56:55 +1000

    finished the education part

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,16 +3,3 @@ import css from "../styles/styles.css";
 if (module.hot) {
   module.hot.accept();
 }
-console.log("bundle this js file");
-
-const obj = {
-  name: "Ruichi",
-  age: 12
-};
-
-const people = {
-  ...obj,
-  gender: "female"
-};
-
-console.log(people);
